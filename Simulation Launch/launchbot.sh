93eeddb91b864be0053b80539da5aa4cf46fd16c
mbm64 <48634854+mbm64@users.noreply.github.com>
2020-01-07 11:23:55 -0500

    Update launchbot.sh

--- Simulation Launch/launchbot.sh
+++ Simulation Launch/launchbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gnome-terminal -e "roscore"
-sleep 3
+#gnome-terminal -e "roscore"
+#sleep 3
 gnome-terminal -e "roslaunch turtlebot3_gazebo turtlebot3_world.launch"
 sleep 3
 gnome-terminal -e "roslaunch turtlebot3_bringup turtlebot3_remote.launch"
